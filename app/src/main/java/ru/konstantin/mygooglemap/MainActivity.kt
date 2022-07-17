2ce6f82fcc8cab466d697d596d4b26eaa7f561ca
Kuzmin <kuzmin.konstantin@otr.ru>
2022-07-17 18:37:49 +0300

    init

---
+++ app/src/main/java/ru/konstantin/mygooglemap/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.konstantin.mygooglemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a_ches.googlemapsactivity.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
